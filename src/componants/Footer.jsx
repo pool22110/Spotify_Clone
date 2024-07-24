2e11043d7d4c3c4578d610f479ab790de210326d
pool22110 <rajmhatre2110@gmail.com>
2024-07-24 20:59:52 +0530

    footer layout

--- src/componants/Footer.jsx
+++ src/componants/Footer.jsx
@@ -9,4 +9,11 @@ export default function Footer() {
 
 const Container = styled.div`
 background-color: #181818;
-height: 100%; ;`
+height: 100%;
+width:100%;
+border-top:1px solid #282828;
+display:grid;
+grid-temple-columns:1fr 2fr 1fr;
+algin-items:center;
+justify-content:center;
+padding:0rem `
